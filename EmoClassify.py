a9bd745dae8cb070c09f302ad4f87b0ddad7f979
v-penlu <laupeng1989@gmail.com>
2015-03-09 21:55:03 +0800

    1-d convolutional layer and coscos^2 layer gradient check OK ^^

--- EmoClassify.py
+++ EmoClassify.py
@@ -1,21 +1,28 @@
 __author__ = 'Liu'
-import itertools
 import numpy as np
 import SequenceToSequence as STS
 from scipy.spatial.distance import cosine
 import math
+import operator
 real = np.float64
+
+
+def accumulate(num_list):
+    acc_list = []
+    for i in range(len(num_list)):
+        acc_list.append(sum(num_list[:i + 1]))
+    return acc_list
 
 
 def Calculate_frame_idx(time_steps, pool_len):
     num_frames = [time_steps / pool_len] * pool_len
     num_frames[-1] += time_steps % pool_len
-    frame_idx = itertools.accumulate(num_frames)
+    frame_idx = accumulate(num_frames)
     frame_idx.insert(0, 0)
     return frame_idx
 
 
-def Pool_feed_forward(in_seq_1, in_seq_2, pool_len=0, average=False):
+def Pool_feed_forward(in_seq_1, in_seq_2, pool_len=0, average=True):
     """Maximize cosine distance between different emotion class and minimize cosine
     distance between same emotion class.
     :param in_seq_1:
@@ -28,10 +35,8 @@ def Pool_feed_forward(in_seq_1, in_seq_2, pool_len=0, average=False):
                     if pool_len=k and average=False, max pooling
     :return:
     """
-    feature_vec_1 = 0
-    feature_vec_2 = 0
-    max_idx_1 = 0
-    max_idx_2 = 0
+    max_idx_1 = []
+    max_idx_2 = []
     if pool_len == 0:
         feature_vec_1 = 0
         feature_vec_2 = 0
@@ -42,16 +47,18 @@ def Pool_feed_forward(in_seq_1, in_seq_2, pool_len=0, average=False):
         if average:
             feature_vec_1 /= len(in_seq_1)
             feature_vec_2 /= len(in_seq_2)
+        feature_vec_1 = feature_vec_1.reshape(-1)
+        feature_vec_2 = feature_vec_2.reshape(-1)
     else:
-        feature_dim = in_seq_2[0].shape[0]
+        feature_dim = in_seq_1[0].shape[0]
         frame_idx_1 = Calculate_frame_idx(len(in_seq_1), pool_len)
         frame_idx_2 = Calculate_frame_idx(len(in_seq_2), pool_len)
         feature_mtx_1 = np.zeros((len(in_seq_1), feature_dim), dtype=real)
         feature_mtx_2 = np.zeros((len(in_seq_2), feature_dim), dtype=real)
         for i, v in enumerate(in_seq_1):
-            feature_mtx_1[i] = in_seq_1[i][0]
+            feature_mtx_1[i] = in_seq_1[i][:, 0]
         for i, v in enumerate(in_seq_2):
-            feature_mtx_2[i] = in_seq_2[i][0]
+            feature_mtx_2[i] = in_seq_2[i][:, 0]
         feature_vec_1 = np.zeros((feature_dim * pool_len), dtype=real)
         feature_vec_2 = np.zeros((feature_dim * pool_len), dtype=real)
         if average:
@@ -68,8 +75,10 @@ def Pool_feed_forward(in_seq_1, in_seq_2, pool_len=0, average=False):
                 feature_vec_2[s: e] = np.max(feature_mtx_2[frame_idx_2[i]: frame_idx_2[i + 1]], axis=0)
                 # In max pooling back propagation, only errors only propagate to
                 # max units.
-                max_idx_1 = np.argmax(feature_mtx_1[frame_idx_1[i]: frame_idx_1[i + 1]], axis=0)
-                max_idx_2 = np.argmax(feature_mtx_2[frame_idx_2[i]: frame_idx_2[i + 1]], axis=0)
+                max_idx_1.append(np.argmax(feature_mtx_1[frame_idx_1[i]: frame_idx_1[i + 1]],
+                                           axis=0) + frame_idx_1[i])
+                max_idx_2.append(np.argmax(feature_mtx_2[frame_idx_2[i]: frame_idx_2[i + 1]],
+                                           axis=0) + frame_idx_2[i])
     return feature_vec_1, feature_vec_2, max_idx_1, max_idx_2
 
 
@@ -98,9 +107,11 @@ def Pool_feed_backward(input_errs_1, input_errs_2, time_steps_1, time_steps_2,
                 s = feature_dim * i
                 e = s + feature_dim
                 for j in range(frame_idx_1[i], frame_idx_1[i + 1]):
-                    Dl_pool_1[j] = input_errs_1[s: e].reshape(feature_dim, 1)
+                    Dl_pool_1[j] = (input_errs_1[s: e].reshape(feature_dim, 1) /
+                                    (frame_idx_1[i + 1] - frame_idx_1[i]))
                 for j in range(frame_idx_2[i], frame_idx_2[i + 1]):
-                    Dl_pool_2[j] = input_errs_2[s: e].reshape(feature_dim, 1)
+                    Dl_pool_2[j] = (input_errs_2[s: e].reshape(feature_dim, 1) /
+                                    (frame_idx_2[i + 1] - frame_idx_2[i]))
         else:
             Dl_mtx_1 = np.zeros((time_steps_1, feature_dim))
             Dl_mtx_2 = np.zeros((time_steps_2, feature_dim))
@@ -119,20 +130,24 @@ def Pool_feed_backward(input_errs_1, input_errs_2, time_steps_1, time_steps_2,
     return Dl_pool_1, Dl_pool_2
 
 
-def Cos_feed_forward(vec_1, vec_2, is_posative):
+def Cos_feed_forward_backward(vec_1, vec_2, is_posative):
     vec_1_pow = vec_1.dot(vec_1)
     vec_2_pow = vec_2.dot(vec_2)
     vec_1_norm = math.sqrt(vec_1_pow)
     vec_2_norm = math.sqrt(vec_2_pow)
     vec_1_2_p = vec_1.dot(vec_2)
     cos = vec_1_2_p / (vec_1_norm * vec_2_norm)
+    # cos = 1 - cosine(vec_1, vec_2)
+    DCos_1 = vec_2 / (vec_1_norm * vec_2_norm) - vec_1 * cos / vec_1_pow
+    DCos_2 = vec_1 / (vec_1_norm * vec_2_norm) - vec_2 * cos / vec_2_pow
     if is_posative:
         loss = (1 - cos) / 2
+        Dl_1 = -0.5 * DCos_1
+        Dl_2 = -0.5 * DCos_2
     else:
         loss = cos * cos
-    cos2 = 1 - cosine(vec_1, vec_2)
-    Dl_1 = vec_2 / (vec_1_norm * vec_2_norm) - vec_1 * cos / vec_1_pow
-    Dl_2 = vec_1 / (vec_1_norm * vec_2_norm) - vec_2 * cos / vec_2_pow
+        Dl_1 = 2 * cos * DCos_1
+        Dl_2 = 2 * cos * DCos_2
     return loss, Dl_1, Dl_2
 
 
--- GradientCheck.py
+++ GradientCheck.py
@@ -1,9 +1,11 @@
 __author__ = 'v-penlu'
 import numpy as np
+import EmoClassify as EC
 import math
 from numpy.linalg import norm
 import SequenceToSequence as STS
 import LanguageModel as LM
+import os
 real = np.float64
 
 check_STS_2vocab = 0
@@ -11,7 +13,9 @@ check_STS_1vocab = 0
 check_LM = 0
 check_softmax = 0
 check_Generage = 0
-check_Beamsearch = 1
+check_Beamsearch = 0
+check_Cos = 0
+check_Pool = 1
 rng = np.random.RandomState()
 if check_STS_2vocab:
     em_time_steps = 11
@@ -119,3 +123,156 @@ if check_softmax:
     np.savetxt("threshold.txt", threshold)
     if np.sum(np.abs(diff) > threshold) > 0:
         print "softmax check failed"
+
+if check_Cos:
+    out_dir = "debug"
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    else:
+        files = os.listdir(out_dir)
+        for f in files:
+            os.remove(os.path.join(out_dir, f))
+    vec_1 = rng.uniform(low=0, high=1, size=10)
+    vec_2 = rng.uniform(low=0, high=1, size=10)
+    _, Dl_1, Dl_2 = EC.Cos_feed_forward_backward(vec_1, vec_2, False)
+    pertub = 1e-6
+    thres = 1e-5
+    vec_1_diff_m = np.zeros(vec_1.shape, dtype=real)
+    vec_1_diff_a = np.zeros(vec_1.shape, dtype=real)
+    for i in range(vec_1.shape[0]):
+        vec_1[i] += pertub
+        vec_1_diff_a[i], _, _ = EC.Cos_feed_forward_backward(vec_1, vec_2, False)
+        vec_1[i] -= 2 * pertub
+        vec_1_diff_m[i], _, _ = EC.Cos_feed_forward_backward(vec_1, vec_2, False)
+        vec_1[i] += pertub
+    numDl_1 = (vec_1_diff_a - vec_1_diff_m) / (2 * pertub)
+    vec_2_diff_m = np.zeros(vec_2.shape, dtype=real)
+    vec_2_diff_a = np.zeros(vec_2.shape, dtype=real)
+    for i in range(vec_2.shape[0]):
+        vec_2[i] += pertub
+        vec_2_diff_a[i], _, _ = EC.Cos_feed_forward_backward(vec_1, vec_2, False)
+        vec_2[i] -= 2 * pertub
+        vec_2_diff_m[i], _, _ = EC.Cos_feed_forward_backward(vec_1, vec_2, False)
+    numDl_2 = (vec_2_diff_a - vec_2_diff_m) / (2 * pertub)
+    diff_1 = Dl_1 - numDl_1
+    diff_2 = Dl_2 - numDl_2
+    diff = [diff_1, diff_2]
+    name = ["vec_1", "vec_2"]
+    algDg = [Dl_1, Dl_2]
+    numDg = [numDl_1, numDl_2]
+    for i in range(2):
+        if np.sum(np.abs(diff[i]) > thres) > 0:
+            formatted_name = " " * (24 - len(name[i])) + name[i]
+            print "%s\tgradient check failed" % formatted_name
+            save_diff_name = "%s.failed.diff.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_diff_name), diff[i], "%+.6e")
+            save_algDg_name = "%s.failed.algDg.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_algDg_name), algDg[i], "%+.6e")
+            save_numDg_name = "%s.failed.numDg.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_numDg_name), numDg[i], "%+.6e")
+        else:
+            formatted_name = " " * (24 - len(name[i])) + name[i]
+            print "%s\tgradient check succeeded" % formatted_name
+            save_diff_name = "%s.succeeded.diff.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_diff_name), diff[i], "%+.6e")
+            save_algDg_name = "%s.succeeded.algDg.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_algDg_name), algDg[i], "%+.6e")
+            save_numDg_name = "%s.succeeded.numDg.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_numDg_name), numDg[i], "%+.6e")
+
+
+def Pool_feed_forward_backward(in_seq_1, in_seq_2, weights_1, weights_2, is_pos,
+                               pool_len=0, average=False):
+    ts_1 = len(in_seq_1)
+    ts_2 = len(in_seq_2)
+    in_1 = []
+    in_2 = []
+    for i in range(ts_1):
+        in_1.append(weights_1 * in_seq_1[i])
+    for i in range(ts_2):
+        in_2.append(weights_2 * in_seq_2[i])
+    v_1, v_2, max_idx_1, max_idx_2 = EC.Pool_feed_forward(in_1, in_2, pool_len, average)
+    loss, Dl_1, Dl_2 = EC.Cos_feed_forward_backward(v_1, v_2, is_pos)
+    Dl_pool_1, Dl_pool_2 = EC.Pool_feed_backward(Dl_1, Dl_2, ts_1, ts_2, max_idx_1,
+                                                 max_idx_2, pool_len, average)
+    Dw_1 = np.zeros(weights_1.shape, dtype=real)
+    Dw_2 = np.zeros(weights_2.shape, dtype=real)
+    for i in range(ts_1):
+        Dw_1 += Dl_pool_1[i] * in_seq_1[i]
+    for i in range(ts_2):
+        Dw_2 += Dl_pool_2[i] * in_seq_2[i]
+    return loss, Dw_1, Dw_2
+
+if check_Pool:
+    out_dir = "debug"
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    else:
+        files = os.listdir(out_dir)
+        for f in files:
+            os.remove(os.path.join(out_dir, f))
+    dim = 5
+    len_1 = 17
+    len_2 = 12
+    seq_1 = []
+    seq_2 = []
+    is_pos = True
+    pool_len = 3
+    average = False
+    for i in range(len_1):
+        seq_1.append(rng.uniform(low=0, high=1, size=(dim, 1)))
+    for i in range(len_2):
+        seq_2.append(rng.uniform(low=0, high=1, size=(dim, 1)))
+    weights_1 = rng.uniform(low=-0.1, high=0.1, size=(dim, 1))
+    weights_2 = rng.uniform(low=-0.1, high=0.1, size=(dim, 1))
+    loss, Dw_1, Dw_2 = Pool_feed_forward_backward(seq_1, seq_2, weights_1, weights_2,
+                                                  is_pos, pool_len, average)
+    pertub = 1e-6
+    thres = 1e-5
+    w_1_diff_m = np.zeros(weights_1.shape, dtype=real)
+    w_1_diff_a = np.zeros(weights_1.shape, dtype=real)
+    for i in range(weights_1.shape[0]):
+        weights_1[i] += pertub
+        w_1_diff_a[i], _, _ = Pool_feed_forward_backward(seq_1, seq_2, weights_1,
+                                                         weights_2, is_pos, pool_len, average)
+        weights_1[i] -= 2 * pertub
+        w_1_diff_m[i], _, _ = Pool_feed_forward_backward(seq_1, seq_2, weights_1,
+                                                         weights_2, is_pos, pool_len, average)
+        weights_1[i] += pertub
+    numDw_1 = (w_1_diff_a - w_1_diff_m) / (2 * pertub)
+    w_2_diff_m = np.zeros(weights_2.shape, dtype=real)
+    w_2_diff_a = np.zeros(weights_2.shape, dtype=real)
+    for i in range(weights_2.shape[0]):
+        weights_2[i] += pertub
+        w_2_diff_a[i], _, _ = Pool_feed_forward_backward(seq_1, seq_2, weights_1,
+                                                         weights_2, is_pos, pool_len, average)
+        weights_2[i] -= 2 * pertub
+        w_2_diff_m[i], _, _ = Pool_feed_forward_backward(seq_1, seq_2, weights_1,
+                                                         weights_2, is_pos, pool_len, average)
+        weights_2[i] += pertub
+    numDw_2 = (w_2_diff_a - w_2_diff_m) / (2 * pertub)
+    diff_1 = Dw_1 - numDw_1
+    diff_2 = Dw_2 - numDw_2
+    diff = [diff_1, diff_2]
+    name = ["weights_1", "weights_2"]
+    algDg = [Dw_1, Dw_2]
+    numDg = [numDw_1, numDw_2]
+    for i in range(2):
+        if np.sum(np.abs(diff[i]) > thres) > 0:
+            formatted_name = " " * (24 - len(name[i])) + name[i]
+            print "%s\tgradient check failed" % formatted_name
+            save_diff_name = "%s.failed.diff.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_diff_name), diff[i], "%+.6e")
+            save_algDg_name = "%s.failed.algDg.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_algDg_name), algDg[i], "%+.6e")
+            save_numDg_name = "%s.failed.numDg.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_numDg_name), numDg[i], "%+.6e")
+        else:
+            formatted_name = " " * (24 - len(name[i])) + name[i]
+            print "%s\tgradient check succeeded" % formatted_name
+            save_diff_name = "%s.succeeded.diff.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_diff_name), diff[i], "%+.6e")
+            save_algDg_name = "%s.succeeded.algDg.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_algDg_name), algDg[i], "%+.6e")
+            save_numDg_name = "%s.succeeded.numDg.txt" % name[i]
+            np.savetxt(os.path.join("debug", save_numDg_name), numDg[i], "%+.6e")
