696f6c0d0b248a3a9bb8d62d96ea582e9360edf8
v-penlu <laupeng1989@gmail.com>
2015-03-16 20:22:25 +0800

    New eval method

--- Emo_eval.py
+++ Emo_eval.py
@@ -1,5 +1,6 @@
 __author__ = 'v-penlu'
 import EmoClassify as EC
+from scipy.spatial.distance import cosine
 import cPickle
 import os
 import numpy as np
@@ -31,9 +32,12 @@ def output_arff(data_dic, csv_dir, arff_name, selected_emo=[]):
     arff = open(arff_name, "w")
     printed = False
     str_format = ""
+    avg_vec = {}
+    emo_vec = {}
     for emo, wav_list in data_dic.items():
         if len(selected_emo) != 0 and emo not in selected_emo:
             continue
+        vec_l = []
         for wav in wav_list:
             csv = os.path.join(csv_dir, ("Session%d" % wav[0]), wav[1],
                                ("%s.csv" % wav[2]))
@@ -45,10 +49,51 @@ def output_arff(data_dic, csv_dir, arff_name, selected_emo=[]):
                 print_header(arff, vec.shape[0], selected_emo)
                 str_format = "%.6f," * vec.shape[0] + "%s"
                 printed = True
+            vec_l.append(vec)
             l = vec.tolist()
             l.append(emo)
             print >> arff, str_format % tuple(l)
+        arr = np.asarray(vec_l)
+        avg_vec[emo] = np.mean(arr, axis=0)
+        emo_vec[emo] = vec_l
     arff.close()
+    pkl_name = os.path.basename(arff_name)
+    pkl_name = os.path.splitext(pkl_name)[0] + '_avg.pkl'
+    pkl = open(pkl_name, "wb")
+    cPickle.dump(avg_vec, pkl)
+    pkl.close()
+    return emo_vec, avg_vec
+
+
+def calculate_error(emo_vec, avg_vec):
+    assign = {}
+    emo_list = emo_vec.keys()
+    for emo in emo_list:
+        assign[emo] = np.zeros((len(emo_list)), dtype=np.int)
+        for vec in emo_vec[emo]:
+            max_val = -1
+            max_idx = -1
+            for i, avg_emo in enumerate(emo_list):
+                cos = 1 - cosine(vec, avg_vec[avg_emo])
+                if cos > max_val:
+                    max_val = cos
+                    max_idx = i
+            assign[emo][max_idx] += 1
+    d_format = "%4d" * len(emo_list)
+    header = "    " + "%4s" * 5
+    accur = []
+    print header % tuple(emo_list)
+    for i, emo in enumerate(emo_list):
+        print emo + (d_format % tuple(assign[emo].tolist()))
+        accur.append(assign[emo][i] / float(np.sum(assign[emo])))
+    for i, emo in enumerate(emo_list):
+        print emo, accur[i]
+    total_acc = 0
+    total = 0
+    for i, emo in enumerate(emo_list):
+        total_acc += assign[emo][i]
+        total += len(emo_vec[emo])
+    print "total_accuracy", total_acc / float(total)
 
 
 if __name__ == "__main__":
@@ -59,5 +104,14 @@ if __name__ == "__main__":
     csv_dir = "D:\IEMOCAP_full_release\emobase"
     train_dic, valid_dic, test_dic = load_pkl(test_dic_n, 3)
     params = load_pkl(params_n)
-    output_arff(train_dic, csv_dir, train_arff_n, test_dic.keys())
-    output_arff(test_dic, csv_dir, test_arff_n)
+    _, avg_vec = output_arff(train_dic, csv_dir, train_arff_n, test_dic.keys())
+    emo_vec, _ = output_arff(test_dic, csv_dir, test_arff_n)
+    pkl_n = "test_emo_feature.arff.pkl"
+    pkl = open(pkl_n, "rb")
+    d = cPickle.load(pkl)
+    # for i in range(len(d.keys())):
+    #     for j in range(i, len(d.keys())):
+    #         emo_1 = d.keys()[i]
+    #         emo_2 = d.keys()[j]
+    #         print emo_1, emo_2, cosine(d[emo_1], d[emo_2])
+    calculate_error(emo_vec, avg_vec)
